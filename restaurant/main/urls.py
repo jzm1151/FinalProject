bf50881dc4ba3c0674c035316db64b0fd543bda8
jacobmoore <jakemoore271@outlook.com>
2021-11-19 17:26:39 -0500

    menu update

--- restaurant/main/urls.py
+++ restaurant/main/urls.py
@@ -3,14 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [ 
-	path('', views.home, name='home'),
-	path('menu', views.menu, name='menu'),
-	path('menu/sort/catagory', views.menu_catagory, name='menu_catagory'),	
-	path('menu/sort/name', views.menu_name, name='menu_name'),
-	path('menu/sort/price', views.menu_price, name='menu_price'),
-	path('register', views.register, name='register'),
-	path('logout', views.logout_user, name='logout'),
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('menu', views.menu, name='menu'),
+    path('menu/sort/catagory', views.menu_catagory, name='menu_catagory'),
+    path('menu/sort/name', views.menu_name, name='menu_name'),
+    path('menu/sort/price', views.menu_price, name='menu_price'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout_user, name='logout'),
 ]
 
 if settings.DEBUG:
--- restaurant/main/views.py
+++ restaurant/main/views.py
@@ -6,48 +6,54 @@ from .forms import UserSignUpForm
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
 def home(request):
-	return render(request, 'main/home.html')
+    return render(request, 'main/home.html')
+
 
 def menu(request):
-	menu = MenuItem.objects.all()
-	context = {'menu' : menu}
-	return render(request, 'main/menu.html', context)
+    menu_obj = MenuItem.objects.all()
+    context = {'menu': menu_obj}
+    return render(request, 'main/menu.html', context)
+
 
 def menu_catagory(request):
-	menu = MenuItem.objects.order_by('catagory');
-	context = {'menu' : menu}
-	return render(request, 'main/menu.html', context)
+    menu_obj = MenuItem.objects.order_by('catagory')
+    context = {'menu': menu_obj}
+    return render(request, 'main/menu.html', context)
+
 
 def menu_name(request):
-	menu = MenuItem.objects.order_by('name');
-	context = {'menu' : menu}
-	return render(request, 'main/menu.html', context)
+    menu_obj = MenuItem.objects.order_by('name')
+    context = {'menu': menu_obj}
+    return render(request, 'main/menu.html', context)
+
 
 def menu_price(request):
-	menu = MenuItem.objects.order_by('price');
-	context = {'menu' : menu}
-	return render(request, 'main/menu.html', context)
+    menu_obj = MenuItem.objects.order_by('price')
+    context = {'menu': menu_obj}
+    return render(request, 'main/menu.html', context)
+
 
 def register(request):
-	if request.method == 'POST':
-		form = UserSignUpForm(request.POST)
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
 
-		if form.is_valid():
-			new_user = form.save()
-			user = authenticate(request, username=new_user.username, password=request.POST['password1'])
-			print(new_user.password)
-			print(new_user.username)
-			if user is not None:
-				login(request, user)
-			return render(request, 'main/home.html')
+        if form.is_valid():
+            new_user = form.save()
+            user = authenticate(request, username=new_user.username, password=request.POST['password1'])
 
-	form = UserSignUpForm()
-	context = {'form' : form}
+            if user is not None:
+                login(request, user)
 
-	return render(request, 'main/register.html', context)
+            return render(request, 'main/home.html')
+
+    form = UserSignUpForm()
+    context = {'form': form}
+
+    return render(request, 'main/register.html', context)
+
 
 def logout_user(request):
-	logout(request)
-	return render(request, 'main/home.html')
+    logout(request)
+    return render(request, 'main/home.html')
